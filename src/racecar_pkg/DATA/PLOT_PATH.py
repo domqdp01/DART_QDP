9b7c90411968b7172e81221a9e366975f5b4ce8b
Domenico <domenico.quartodipalo@mail.polimi.it>
2024-12-03 16:56:08 +0100

    Add node to choose whether to use the simulator with or without noise and some path with noise

--- src/racecar_pkg/DATA/PLOT_PATH.py
+++ src/racecar_pkg/DATA/PLOT_PATH.py
@@ -5,12 +5,12 @@ import numpy as np
 
 # file_path = 'car_1_Datarecording_12_03_2024_11_43_26.csv'
 # file_path = 'car_1_Datarecording_12_03_2024_11_46_00.csv'
-file_path = 'car_1_Datarecording_12_03_2024_15_45_25.csv'
-# file_path = '.csv'
-# file_path = '.csv'
-# file_path = '.csv'
-# file_path = '.csv'
-# file_path = '.csv'
+# file_path = 'car_1_Datarecording_12_03_2024_15_45_25.csv'
+# file_path = 'car_1_Datarecording_12_03_2024_15_54_54.csv'
+# file_path = 'car_1_Datarecording_12_03_2024_16_32_22.csv'
+# file_path = 'car_1_Datarecording_12_03_2024_16_34_36.csv'
+# file_path = 'car_1_Datarecording_12_03_2024_16_47_41.csv'
+file_path = 'car_1_Datarecording_12_03_2024_16_53_50.csv'
 
 
 df = pd.read_csv(file_path)
@@ -19,18 +19,30 @@ x = df['vicon x'].values
 y = df['vicon y'].values
 v = df['vel encoder'].values
 t = df['elapsed time sensors'].values
+yaw = df['vicon yaw'].values
+w = df['W (IMU)'].values
 
-plt.subplot(2,1,1)
+plt.subplot(4,1,1)
 plt.plot(x, y,  label='Trajectory',  zorder=1)
-plt.scatter(x[0], y[0], color='red', s=30, label='Starting point', zorder=2)  
-plt.scatter(x[-1], y[-1], color='black', s=30, label='Ending point',  zorder=2)  
+plt.scatter(x[0], y[0], color='red', s=30, label='Starting point', zorder=2)
+plt.scatter(x[-1], y[-1], color='black', s=30, label='Ending point',  zorder=2)
 plt.title("CAR SIMULATOR TRAJECTORY", fontsize=14)
 plt.xlabel("X", fontsize=12)
 plt.ylabel("Y", fontsize=12)
 plt.legend(loc='best')
-plt.subplot(2,1,2)
-plt.plot(t, v,  label='velocity') 
+plt.subplot(4,1,2)
+plt.plot(t, v,  label='velocity')
 plt.xlabel("t", fontsize=12)
 plt.ylabel("V", fontsize=12)
+plt.legend(loc='best')
+plt.subplot(4,1,3)
+plt.plot(t, yaw, label='Yaw')
+plt.xlabel("t", fontsize=12)
+plt.ylabel("YAW", fontsize=12)
+plt.legend(loc='best')
+plt.subplot(4,1,4)
+plt.plot(t, w, label='W')
+plt.xlabel("t", fontsize=12)
+plt.ylabel("W", fontsize=12)
 plt.legend(loc='best')
 plt.show()
